d71dcd817f566934a97845d7d97f3f573946c8c8
nivr4 <63297412+nivr4@users.noreply.github.com>
2021-03-18 18:19:04 +0200

    Update state.ts

--- src/part3/state.ts
+++ src/part3/state.ts
@@ -1,3 +1,7 @@
 export type State<S, A> = (initialState: S) => [S, A];
 
-export const bind = undefined;
+export const bind = <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) : State<S, B> =>{
+      const y= state(state.arguments[0])
+    return f(y[1]) ;
+
+}
