06dd59d6f21d4e61ce3064f2b5f2f19ff84a74fb
benaomri <63296602+benaomri@users.noreply.github.com>
2021-03-18 23:24:22 +0200

    bind

--- src/part3/state.ts
+++ src/part3/state.ts
@@ -1,3 +1,10 @@
+import { stat } from "node:fs";
+
 export type State<S, A> = (initialState: S) => [S, A];
 
-export const bind = undefined;
+export const bind = <S,A,B>(state:State<S, A>,f:(x:A)=>State<S, B>) : State<S, B> => (initialState:S)=>{
+    const [futureState,outcome] = state(initialState);
+    const result : State<S, B> = f(outcome);
+    return result(futureState)
+}
+
