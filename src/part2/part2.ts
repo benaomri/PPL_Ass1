c70d78f93d2aa07a14bd886af9c0e1037e29e044
benaomri <63296602+benaomri@users.noreply.github.com>
2021-03-18 23:25:06 +0200

    q1

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -2,21 +2,22 @@ import * as R from "ramda";
 
 const stringToArray = R.split("");
 
+
 /* Question 1 */
-export const countVowels: (str:string) =>number = (str) => {
+export const countVowels=R.pipe(
+    (str:string) =>stringToArray(str),
+    (array:string[])=>array.filter((t)=>{
+       if('aeiouAEIOU'.includes(t)) return  true;
+       return false;}),
+    (array :string[])=>array.length
+) 
 
-    const len= (stringToArray(str).filter((t:string) =>
-            {if ('aeiou'.includes(t)) return true; })).length;
-            console.log("len",len)
-            if (len)  return len;
-            return 0;
 
-}
 
 /* Question 2 */
 export const runLengthEncoding: (str:string) => string = (str) => {
-    let string = stringToArray(str),
-      counter = 1,
+    const string = stringToArray(str),
+      counter = [1],
       compString = string.reduce(function (
         accumulator,
         currentValue,
@@ -26,12 +27,14 @@ export const runLengthEncoding: (str:string) => string = (str) => {
 
         if (currentValue === array[currentIndex + 1]) {
           //increment and move on
-          counter++;
+          counter[0]++;
+        //   counter=counter+1;
           return accumulator;
         } else {
           //save letter and number
           accumulator += (currentValue + counter);
-          counter = 1;
+          counter[0]=1;
+        //   counter = 1;
           return accumulator;
         }
 
@@ -44,5 +47,5 @@ export const isPaired:(str:string) =>boolean = (str)=>{
     return true;
 
 };
-console.log(countVowels("hello"))
+console.log(countVowels("hEllo"))
 console.log(runLengthEncoding("AAaaaabbbccc"))
