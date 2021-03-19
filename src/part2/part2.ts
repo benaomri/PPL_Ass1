59bbed2e3419815819b2b213d84e820d900e8b14
benaomri <63296602+benaomri@users.noreply.github.com>
2021-03-19 12:49:12 +0200

    beauty and the beast

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -6,39 +6,22 @@ const stringToArray = R.split("");
 /* Question 1 */
 export const countVowels=R.pipe(
     (str:string) =>stringToArray(str),
-    (array:string[])=>array.filter((t)=>{
+     R.filter((t:string)=>{
         return 'aeiouAEIOU'.includes(t)}),
-    (array :string[])=>array.length) 
+     R.length); 
 
 
 
 /* Question 2 */
-export const runLengthEncoding: (str:string) => string = (str) => {
-    const string = stringToArray(str),
-      counter = [1],
-      compString = string.reduce(function (
-        accumulator,
-        currentValue,
-        currentIndex,
-        array
-      ) {
-
-        if (currentValue === array[currentIndex + 1]) {
-          //increment and move on
-          counter[0]++;
-        //   counter=counter+1;
-          return accumulator;
-        } else {
-          //save letter and number
-          accumulator += (currentValue + counter);
-          counter[0]=1;
-        //   counter = 1;
-          return accumulator;
-        }
-
-      }, "");
-  return  compString;
-}
+export const runLengthEncoding =R.pipe(
+      (str:string)=>stringToArray(str),
+      R.groupWith(R.equals),
+      R.map((str)=>(str.length>=2) ? [str[0],str.length] : str[0]),
+      R.join(""),
+      (str:string)=>stringToArray(str),
+      R.filter(str=>str!=","),
+      R.join("")
+      );
 
 /* Question 3 */
 export const isPaired:(str:string) =>boolean = (str)=>{
